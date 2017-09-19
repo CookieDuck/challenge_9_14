e527b4f7337cfa2065d6f3ea1f6117fef0f0badd
Paul Ganster <paul.ganster@gmail.com>
2017-09-18 20:46:58 -0600

    Added tests

--- src/test/java/com/ganster/challenge/ControllerTest.java
+++ src/test/java/com/ganster/challenge/ControllerTest.java
@@ -1,8 +1,11 @@
 package com.ganster.challenge;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -13,6 +16,8 @@ import static org.junit.Assert.assertTrue;
 @RunWith(JUnit4.class)
 public class ControllerTest {
     private final Controller controller = new Controller();
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     public void test1() {
@@ -33,12 +38,85 @@ public class ControllerTest {
     }
 
     @Test
-    public void test2() {
-        
+    public void test2() throws IOException {
+//        Node root = Node.builder().
+//                value(1).
+//                left(Node.builder().value(2).left(
+//                        Node.builder().value(3).build()
+//                ).right(
+//                        Node.builder().value(4).build()
+//                ).build())
+//                .right(Node.builder().value(5).left(
+//                        Node.builder().value(6).build()
+//                ).right(
+//                        Node.builder().value(7).build()
+//                ).build()).build();
+
+        Node root = objectMapper.readValue("{\n" +
+                "  \"value\": 6,\n" +
+                "  \"left\": {\n" +
+                "    \"value\": 3,\n" +
+                "    \"left\": {\n" +
+                "      \"value\": 4,\n" +
+                "      \"left\": null,\n" +
+                "      \"right\": null\n" +
+                "    },\n" +
+                "    \"right\": {\n" +
+                "      \"value\": 5,\n" +
+                "      \"left\": null,\n" +
+                "      \"right\": null\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"right\": {\n" +
+                "    \"value\": 7,\n" +
+                "    \"left\": null,\n" +
+                "    \"right\": null\n" +
+                "  }\n" +
+                "}", Node.class);
+        String output = controller.getTopLevel(root);
+        assertEquals("4367", output);
     }
 
     @Test
-    public void test3() {
-
+    public void test3() throws IOException {
+        Node root = objectMapper.readValue("{\n" +
+                "  \"value\": 10,\n" +
+                "  \"left\": {\n" +
+                "    \"value\": 9,\n" +
+                "    \"left\": {\n" +
+                "      \"value\": 7,\n" +
+                "      \"left\": null,\n" +
+                "      \"right\": null\n" +
+                "    },\n" +
+                "    \"right\": {\n" +
+                "      \"value\": 6,\n" +
+                "      \"left\": {\n" +
+                "        \"value\": 5,\n" +
+                "        \"left\": null,\n" +
+                "        \"right\": null\n" +
+                "      },\n" +
+                "      \"right\": {\n" +
+                "        \"value\": 4,\n" +
+                "        \"left\": {\n" +
+                "          \"value\": 3,\n" +
+                "          \"left\": null,\n" +
+                "          \"right\": null\n" +
+                "        },\n" +
+                "        \"right\": {\n" +
+                "          \"value\": 2,\n" +
+                "          \"left\": null,\n" +
+                "          \"right\": null\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"right\": {\n" +
+                "    \"value\": 8,\n" +
+                "    \"left\": null,\n" +
+                "    \"right\": null\n" +
+                "  }\n" +
+                "}", Node.class);
+        String output = controller.getTopLevel(root);
+        assertEquals("791082", output);
     }
 }
